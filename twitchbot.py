4e0427d2093a243e19c248f97f1495d5c6a2697f
iago <96024218+iagohenrique2009@users.noreply.github.com>
2024-11-13 10:38:13 -0300

    Update twitchbot.py

--- twitchbot.py
+++ twitchbot.py
@@ -2,18 +2,17 @@ import asyncio
 import aiohttp
 from urllib.parse import quote
 from twitchio.ext import commands
-from aiohttp import web
 import os
 
 # Configuração direta dos tokens e IDs
-ACCESS_TOKEN = 'zw1zv7ltonhexc13m0r8b3rb13h5g5'
-REFRESH_TOKEN = 'oc8g5j7yk8869aymsbtgtbghfs5awwzlkjfsgc7tam4r1oh6co'
-CLIENT_ID = 'gp762nuuoqcoxypju8c569th9wz7q5'
+ACCESS_TOKEN = 'seu_token'
+REFRESH_TOKEN = 'seu_refresh_token'
+CLIENT_ID = 'seu_client_id'
 
 # Inicialize o bot com o token e os canais
 bot = commands.Bot(
-    token=ACCESS_TOKEN,  # Token de acesso direto no código
-    prefix='!',  # Prefixo para comandos
+    token=ACCESS_TOKEN,
+    prefix='!',
     initial_channels=['nevertoolatel2', 'iagonumeros']  # Lista de canais para o bot
 )
 
@@ -59,24 +58,6 @@ async def imagem(ctx, *, img_name: str):
     else:
         await ctx.send(f"Desculpe, não foi possível gerar a imagem '{img_name}' no tempo esperado. Tente novamente mais tarde.")
 
-# Função assíncrona para o servidor HTTP usando aiohttp
-async def start_web_server():
-    async def handle(request):
-        return web.Response(text="Bot da Twitch está ativo!")
-
-    app = web.Application()
-    app.router.add_get("/", handle)
-
-    # Porta definida pelo Render ou padrão 8080
-    port = int(os.getenv("PORT", 8080))
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
-    print(f"Servidor HTTP rodando na porta {port}")
-
-# Inicie o bot e o servidor HTTP
+# Inicie o bot da Twitch
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_web_server())  # Inicia o servidor HTTP
-    bot.run()  # Inicia o bot da Twitch
+    bot.run()
